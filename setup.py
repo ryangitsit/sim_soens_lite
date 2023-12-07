f10e4e89eb70c9d6b1123b23b68c4e33a5106045
ryangitsit <rmoloughlin11@gmail.com>
2023-12-06 19:31:18 -0700

    dependencies

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 from setuptools import setup, find_packages
+import os
+
+required = [
+    "numpy",
+    "matplotlib",
+    "seaborn",
+    "networkx",
+    "keras",
+    "scikit-learn",
+    "tensorflow",
+    'markupsafe',
+    "brian2"
+]
 
 setup(
     packages=find_packages(where="src"),
@@ -6,4 +19,5 @@ setup(
     package_dir={"sim_soens_lite":"src/sim_soens_lite"},
     package_data={'sim_soens_lite' :["sim_soens_lite/soen_sim_data/*.soen"]},
     include_package_data=True,
+    install_requires=required,
     )
